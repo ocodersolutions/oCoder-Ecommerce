523ea86ed0d3803527ea142ae1e397e448eb8293
Mr Truong <truongdinh94@gmail.com>
2016-07-11 11:16:20 +0700

    page delete

--- wp-content/themes/storefront/page.php
+++ wp-content/themes/storefront/page.php
@@ -11,7 +11,26 @@
  */
 
 get_header(); ?>
-	
+
+	<div class="main">
+		<div class="container">
+			<?php while ( have_posts() ) : the_post();
+
+				do_action( 'storefront_page_before' );
+
+				get_template_part( 'content', 'page' );
+
+				/**
+				 * Functions hooked in to storefront_page_after action
+				 *
+				 * @hooked storefront_display_comments - 10
+				 */
+				do_action( 'storefront_page_after' );
+
+			endwhile; // End of the loop. ?>
+		</div>
+	</div>
+
 <?php
 do_action( 'storefront_sidebar' );
 get_footer();
