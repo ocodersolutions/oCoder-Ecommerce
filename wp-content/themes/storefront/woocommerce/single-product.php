d3f5c5e7ac24ade999a59297477c48fdaf05cb29
Phan Văn Huy Hoàng <pvhuyhoang166@gmail.com>
2016-07-19 08:07:12 +0700

    singlepage

--- wp-content/themes/storefront/woocommerce/single-product.php
+++ wp-content/themes/storefront/woocommerce/single-product.php
@@ -21,59 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 get_header( 'shop' );?>
-		<!-- <?php
-			/**
-			 * woocommerce_before_main_content hook.
-			 *
-			 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-			 * @hooked woocommerce_breadcrumb - 20
-			 */
-			//do_action( 'woocommerce_before_main_content' );
-		?>
-			<div class="single">
-				<div class="container">
-					<div class="single-grids">
-<<<<<<< HEAD
-						<div class="col-md-9">
-						<?php //while ( have_posts() ) : the_post(); ?>
-
-							<?php //wc_get_template_part( 'content', 'single-product' ); ?>
-
-						<?php //endwhile; // end of the loop. ?>
-=======
-						<div class="row">
-							<div class="col-md-9">
-								<?php while ( have_posts() ) : the_post(); ?>
-
-									<?php wc_get_template_part( 'content', 'single-product' ); ?>
-
-								<?php endwhile; // end of the loop. ?>
-							</div>
->>>>>>> 2480d3152b606ed340b95ef92857f76981f14af4
-						</div>
-					</div>
-				</div>
-			</div>
-
-		<?php
-
-			/**
-			 * woocommerce_after_main_content hook.
-			 *
-			 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-			 */
-			//do_action( 'woocommerce_after_main_content' );
-		?>
-
-		<?php
-			/**
-			 * woocommerce_sidebar hook.
-			 *
-			 * @hooked woocommerce_get_sidebar - 10
-			 */
-			//do_action( 'woocommerce_sidebar' );
-		?> -->
-	
+		
 
 <div class="single">
 		<!-- container -->
